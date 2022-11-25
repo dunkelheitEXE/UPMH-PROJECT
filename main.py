8ce793dbd95909f186ed5a3ebecb5f9302b5416e
Alan <alangmoreno02@gmail.com>
2022-11-24 20:10:57 -0600

    version 4

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ bot = ChatBot('UPMH-friend')
 # Hola
 # linesahdbhja
 # kjsdajkhasd
+
+# lista = ['Pregunta', 'respuesta del bot', ...]
+words = ['Me siento mal', 'Contamos con una enfermeria en el edificio LT1',
+        'Necesito ayuda emocional', 'Puedes acudir con tus orientadores en su respectivo edificio',
+        '¿Como puedo saber sobre becas', 'Puedes acudir al edificio UD1 con la Lic. Elisa Acuña']
+
+trainer = ListTrainer(bot)
+trainer.train(words)
+
+while True:
+    consulta = input("User: ")
+    respuesta = bot.get_response(consulta)
+    print("Bot: ", respuesta)
