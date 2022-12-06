a3faf59ec364dca679e39e65f6541a860ea0ebdd
Alfred0JHH <alfre.hdez.hdez.ajhh@hmail.com>
2022-12-06 11:03:09 -0600

    6/12/22 11:03 AJHH

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 bot = ChatBot('UPMH-friend')
-# Hola
-# linesahdbhja
-# kjsdajkhasd
-
 # lista = ['Pregunta', 'respuesta del bot', ...]
 words = ['Me siento mal', 'Contamos con una enfermeria en el edificio LT1',
         'Necesito ayuda emocional', 'Puedes acudir con tus orientadores en su respectivo edificio',
@@ -52,8 +48,7 @@ words = ['Me siento mal', 'Contamos con una enfermeria en el edificio LT1',
 
 trainer = ListTrainer(bot)
 trainer.train(words)
-#holapapito
-#Hola bb
+
 while True:
     consulta = input("User: ")
     respuesta = bot.get_response(consulta)
